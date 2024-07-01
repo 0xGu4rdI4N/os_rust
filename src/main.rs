29b0b1ae22477f9ccb77e92c25ce0bcbb24b7d7e
Gu4rdI4N <raunak0831@gmail.com>
2024-07-01 16:35:09 +0530

    new line support and formatting macro added

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ static HELLO: &[u8] = b"Hello World! This is my OS";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     vga_buffer::print_something();
-
+    // panic!("Some panic message");
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    // vga_buffer::panic_print(info);
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -72,7 +72,7 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
+    // fn new_line(&mut self) {/* TODO */}
 }
 impl Writer {
     pub fn write_string(&mut self, s: &str) {
@@ -87,15 +87,57 @@ impl Writer {
         }
     }
 }
+use core::fmt;
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+impl Writer {
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    // fn clear_row(&mut self, row: usize) {/* TODO */}
+}
+impl Writer {
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
 
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        color_code: ColorCode::new(Color::Pink, Color::DarkGray),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     };
 
     writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    writer.write_string("e11o ");
+    writer.write_string("World!");
+    writer.write_string("VGA text mode added successfully");
+    writer.write_byte(b'\n');
+    write!(writer, "--Gu{}rdI{}N", 4,4).unwrap();
 }
+// use core::panic::PanicInfo;
+// pub fn panic_print(panic_info: &PanicInfo){
+//     use core::fmt::Write;
+
+// }
